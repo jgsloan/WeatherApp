c2a3eade0b002fb05c67023f47c5fde834a17ddf
John Sloan <johnsloan@johns-MBP.ultrahub>
2024-10-15 23:49:56 +0100

    Added html, css & js

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
+function getWeather() {
+  const apiKey = "07ece45657498a76b3d89f88200047e4";
+  const city = document.getElementById("city").value;
 
+  if (!city) {
+    alert("Please enter a city");
+    return;
+  }
+  const currentWeatherUrl =
+    "https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}";
+  const forecastUrl =
+    "https://api.openweather.org/data/2.5/forecast?q=${city}&appid=${apiKey}";
+
+  fectch(currentWeatherUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      displayWeather(data);
+    })
+    .catch((error) => {
+      console.error("Error fetching current weather data:, error");
+      alert("Error fetching current weather data. Please try again.");
+    });
+
+  fetch(forecastUrl)
+    .then(response.json())
+    .then((data) => {
+      displayHourlyForecast(data.list);
+    })
+    .catch((error) => {
+      console.error("Error fetching hourly forecast data:", error);
+      alert("Error fetching hourly forecast data. Please try again.");
+    });
+}
